c2c3eb1c91e37311b1fa4444a21f4e1a1e1e5bfa
NYTLAIT <231107618+NYTLAIT@users.noreply.github.com>
2025-11-18 16:14:35 -0800

    implemented while loops

--- checklist.py
+++ checklist.py
@@ -26,26 +26,35 @@ def mark_completed(index):
     item = "√" + read(index)
     update(index, item)
 
+def user_input(prompt):
+    user_input = input(prompt)
+    return user_input
+
 def select(function_code):
     # Create item
     if function_code == "C":
-        input_item = input("Input item:")
+        input_item = user_input("Input item:")
         create(input_item)
 
     # Read item
     elif function_code == "R":
-        item_index = int(input("Index Number?"))
+        item_index = int(user_input("Index Number?"))
 
-        # Remember that item_index must actually exist or our program will crash.
         read(item_index)
 
     # Print all items
     elif function_code == "P":
         list_all_items()
 
+    # Stop code
+    elif function_code == "Q":
+        return False
+
     # Catch all
     else:
         print("Unknown Option")
+    
+    return True
 
 # TEST
 def test():
@@ -74,3 +83,9 @@ def test():
     # Continue until all code is run
 
 test()
+
+running = True
+while running:
+    selection = user_input(
+        "Press C to add to list, R to Read from list, P to display list, and Q to quit")
+    running = select(selection)
